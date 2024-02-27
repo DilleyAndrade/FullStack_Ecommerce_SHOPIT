13d31d5fe8e58b66aafb8cadf4d9e2b1e65d798a
Dilley Andrade <dilleyandrade@gmail.com>
2024-02-27 19:15:11 -0300

    First commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app = express()
+
+dotenv.config({ path: 'backend/config/config.env' })
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  )
+})
