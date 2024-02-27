38319cb77b91fcf8154c7408d2a759555654bf10
Dilley Andrade <dilleyandrade@gmail.com>
2024-02-27 19:44:40 -0300

    Created first route

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+//import all router
+import productRouter from './routes/products.js'
+
 
 const app = express()
 
 dotenv.config({ path: 'backend/config/config.env' })
+
+// "/api/v1" será a url exibida antes de /products ficando /api/v2/products
+app.use("/api/v1", productRouter)
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(
