02a06b1a9fddb00415b3b97973fe795dfeebe56a
Dilley Andrade <dilleyandrade@gmail.com>
2024-02-29 09:57:42 -0300

    Find product by id

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,10 +1,8 @@
 import Product from "../models/product.js"
 
-//View all products => /api/v1/products
+//Get all products => /api/v1/products
 export const getProducts = async (req, res) =>{
-
   const products = await Product.find() 
-
   res.status(200).json({
    products,
   })
@@ -12,10 +10,23 @@ export const getProducts = async (req, res) =>{
 
 //Create new product => /api/v1/admin/products
 export const newProduct = async (req, res) =>{
-
  const product = await Product.create(req.body)
-
  res.status(200).json({
   product,
  })
 }
+
+//Get single product details => /api/v1/products/:id
+export const getProductDetails = async (req, res) =>{
+  const product = await Product.findById(req?.params?.id)
+
+  if(!product) {
+    return res.status(400).json({
+      error: "Product not found",
+    })
+  }
+
+  res.status(200).json({
+   product,
+  })
+ }
