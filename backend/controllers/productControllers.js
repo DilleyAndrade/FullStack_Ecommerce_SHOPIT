48686800b587d1fee6976cf1b7713b3a56a7e54e
Dilley Andrade <dilleyandrade@gmail.com>
2024-02-29 10:12:31 -0300

    Update product function

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,6 @@
 import Product from "../models/product.js"
 
-//Get all products => /api/v1/products
+// Get all products => /api/v1/products
 export const getProducts = async (req, res) =>{
   const products = await Product.find() 
   res.status(200).json({
@@ -8,7 +8,7 @@ export const getProducts = async (req, res) =>{
   })
 }
 
-//Create new product => /api/v1/admin/products
+// Create new product => /api/v1/admin/products
 export const newProduct = async (req, res) =>{
  const product = await Product.create(req.body)
  res.status(200).json({
@@ -16,7 +16,7 @@ export const newProduct = async (req, res) =>{
  })
 }
 
-//Get single product details => /api/v1/products/:id
+// Get single product details => /api/v1/products/:id
 export const getProductDetails = async (req, res) =>{
   const product = await Product.findById(req?.params?.id)
 
@@ -25,6 +25,25 @@ export const getProductDetails = async (req, res) =>{
       error: "Product not found",
     })
   }
+
+  res.status(200).json({
+   product,
+  })
+ }
+
+ // Update product details => /api/v1/products/:id
+export const updateProduct = async (req, res) =>{
+  let product = await Product.findById(req?.params?.id)
+
+  if(!product) {
+    return res.status(400).json({
+      error: "Product not found",
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { 
+    new: true,
+  })
 
   res.status(200).json({
    product,
