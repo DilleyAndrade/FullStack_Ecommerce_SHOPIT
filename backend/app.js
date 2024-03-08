28b50fe3500c581406c0f8223864818fd8bc5cf0
Dilley Andrade <dilleyandrade@gmail.com>
2024-03-08 19:20:34 -0300

    Create erros middleware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDatabase } from './config/dbConnect.js'
 import productRouter from './routes/products.js'
+import errorMiddleware from './middlewares/errors.js'
 
 const app = express()
 
@@ -12,6 +13,8 @@ connectDatabase()
 app.use(express.json())
 
 app.use("/api/v1", productRouter)
+
+app.use(errorMiddleware)
 
 app.listen(process.env.PORT, () => {
   console.log(
--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,11 +1,12 @@
+// Error Handler Class
 class ErrorHandler extends Error {
-  constructor(message,statusCode){
-    super(message)
-    this.statusCode = statusCode
+  constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode
 
-    //create stack property
-    Error.captureStackTrace(thi, this.constructor)
+          //create stack property, Opcional
+      Error.captureStackTrace(this, this.constructor)
   }
 }
 
-export default ErrorHandler
+export default ErrorHandler;
