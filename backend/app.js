c5bbbab555a41188c7bfac43a2429f80bbe12234
Dilley Andrade <dilleyandrade@gmail.com>
2024-03-08 18:33:34 -0300

    Create error handler class

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv'
 import { connectDatabase } from './config/dbConnect.js'
 import productRouter from './routes/products.js'
 
-
-
 const app = express()
 
 dotenv.config({ path: 'backend/config/config.env' })
@@ -14,8 +12,6 @@ connectDatabase()
 app.use(express.json())
 
 app.use("/api/v1", productRouter)
-
-
 
 app.listen(process.env.PORT, () => {
   console.log(
