89a07710593c27cc5aa2f86cdd018fbb89766e48
Dilley Andrade <dilleyandrade@gmail.com>
2024-03-26 19:28:43 -0300

    Implementing Pagination

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -6,12 +6,19 @@ import ErrorHandler from "../util/errorHandler.js"
 // Get all products => /api/v1/products
 export const getProducts = catchAsyncErrors( async (req, res) =>{
 
-  const apiFilters = new APIFilters(Product, req.query).search().filters()
+  const resPerPage = 4
 
+  const apiFilters = new APIFilters(Product, req.query).search().filters()
+  
   let products = await apiFilters.query
+
   let filteredProductsCount = products.length
 
+  apiFilters.pagination(resPerPage)
+  products = await apiFilters.query.clone()
+
   res.status(200).json({
+    resPerPage,
     filteredProductsCount,
     products,
   })
